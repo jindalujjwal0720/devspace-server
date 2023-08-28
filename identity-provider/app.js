50a9d7aa7fee5f423eaa666623ff22a8fe8ff9d5
Ujjwal Jindal <jindalujjwal0720@gmail.com>
2023-08-28 17:44:08 +0530

    initial commit - setup

---
+++ identity-provider/app.js
@@ -0,0 +1,14 @@
+/*
+ * Devspace IIT(ISM) Dhanbad
+ *
+ * This software and related documentation are owned by Devspace IIT(ISM) Dhanbad.
+ * Unauthorized copying, reproduction, or modification via any medium is strictly prohibited.
+ * Proprietary and confidential. All rights reserved.
+ */
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/idp", require("./routes/index"));
+
+module.exports = router;
